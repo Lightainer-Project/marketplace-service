0b131b09a75aa83cc7553f203294b06772a6cb33
Niklas Lausch <support@niki2k1.de>
2019-11-29 17:48:44 +0100

    Updated Packet Schema

--- src/models/packet.ts
+++ src/models/packet.ts
@@ -1,14 +1,28 @@
 import mongoose, { Schema, Document } from "mongoose";
 
 export interface PacketInterface extends Document {
-  title: string;
+  name: string;
+  version: string;
   author: string;
 }
 
 const PacketSchema: Schema = new Schema({
   name: { type: String, required: true },
   version: { type: String, required: true },
-  author: { type: String, required: true }
+  author: { type: String, required: true },
+  installation: { 
+    installType: String,
+    url: String
+  },
+  configs: [
+    {
+      path: String
+    }
+  ],
+  startup: {
+    command: String,
+    autostart: Boolean
+  }
 });
 
 const Packet = mongoose.model<PacketInterface>("Packet", PacketSchema);
